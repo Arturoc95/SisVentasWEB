ac72f73b3f3e60e330a93e69b6fbc36433a44f0e
Arturoc95 <37820741+Arturoc95@users.noreply.github.com>
2018-11-26 14:18:59 -0500

    WEB

---
+++ SisVentasWEB/SisVentasWEB/Controllers/DefaultController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SisVentasWEB.Models;
+using System.Data;
+
+namespace SisVentasWEB.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Articulo()
+        {
+            SisVentasEntitiesArticulo cdb2 = new SisVentasEntitiesArticulo();
+
+            return View(from ass in cdb2.Articulo.ToList() select ass);
+        }
+
+       
+    }
+}
---
+++ SisVentasWEB/SisVentasWEB/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.SqlServer;
+using System.Data;
+using System.EnterpriseServices;
+using SisVentasWEB.Models;
+
+namespace SisVentasWEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            SisVentasEntities cdb = new SisVentasEntities();
+            return View(from emp in cdb.Categoria.ToList()
+                        select emp);
+        }
+
+        public ActionResult About()
+        {
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
